6e43dbd7f208bc898b0ae9fa3485335ea80ff70e
James Tuck <JamesjTuck@gmail.com>
2021-02-06 11:08:40 -0700

    added schema

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercises: [
+        {
+            distance: {
+                type: Number,
+            },
+            duration: {
+                type: Number,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            type: {
+                type: String,
+                trim: true,
+            },
+            weight: {
+                type: Number,
+            },
+        }
+    ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
